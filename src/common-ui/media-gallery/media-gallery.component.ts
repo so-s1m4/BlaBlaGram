9bd0433aea3f7bae69f20858860ae00fca325e86
Maksym <maksym@Maksyms-MacBook-Air.local>
2025-07-31 01:41:32 +0200

    new version of media-gallery

--- src/common-ui/media-gallery/media-gallery.component.ts
+++ src/common-ui/media-gallery/media-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { API_URL } from '../../app/app.config';
 import { SvgIconComponent } from '../../app/utils/svg.component';
 
@@ -9,6 +9,10 @@ import { SvgIconComponent } from '../../app/utils/svg.component';
   styleUrl: './media-gallery.component.css',
 })
 export class MediaGalleryComponent implements OnInit {
+  xDown: any = null
+  yDown: any = null
+
+  @ViewChild('wrapper') wrapper!: ElementRef<HTMLDivElement>;
   API_URL = API_URL;
 
   @Input() media: any[] = [];
@@ -21,8 +25,6 @@ export class MediaGalleryComponent implements OnInit {
   goToMedia(mediaId: string) {
     this.selectedMedia = this.media.find(item => item._id === mediaId);
   }
-
-
   goBack() {
     const currentIndex = this.media.indexOf(this.selectedMedia);
     if (currentIndex > 0) {
@@ -35,7 +37,6 @@ export class MediaGalleryComponent implements OnInit {
       this.selectedMedia = this.media[currentIndex + 1];
     }
   }
-
   onClose() {
     this.close.emit();
   }
@@ -43,10 +44,35 @@ export class MediaGalleryComponent implements OnInit {
     this.delete.emit({ comId, mediaId });
   }
 
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+  swipe(e: TouchEvent, when: string): void {
+
+  const coord: [number, number] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
+  const time = new Date().getTime();
+
+  if (when === 'start') {
+    this.swipeCoord = coord;
+    this.swipeTime = time;
+  }
+  else if (when === 'end') {
+    const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
+    const duration = time - this.swipeTime!;
+  if (duration < 1000
+    && Math.abs(direction[0]) > 30 
+    && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { 
+    if (direction[0] < 0) {
+      this.goForward()
+    } else {
+      this.goBack()
+    }
+    }
+  }
+  }
+  
   ngOnInit() {
     if (this.media.length > 0) {
       this.selectedMedia = this.media[0];
     }
-    console.log(this.media);
   }
 }
