903599f860644bc988e1770f41da9622a53cc709
so-s1m4 <maks.rvachov.at@gmail.com>
2025-08-01 02:15:50 +0200

    fix

--- src/common-ui/media-gallery/media-gallery.component.ts
+++ src/common-ui/media-gallery/media-gallery.component.ts
@@ -1,4 +1,13 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { API_URL } from '../../app/app.config';
 import { SvgIconComponent } from '../../app/utils/svg.component';
 
@@ -9,8 +18,8 @@ import { SvgIconComponent } from '../../app/utils/svg.component';
   styleUrl: './media-gallery.component.css',
 })
 export class MediaGalleryComponent implements OnInit {
-  xDown: any = null
-  yDown: any = null
+  xDown: any = null;
+  yDown: any = null;
 
   @ViewChild('wrapper') wrapper!: ElementRef<HTMLDivElement>;
   API_URL = API_URL;
@@ -19,11 +28,10 @@ export class MediaGalleryComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
   @Output() delete = new EventEmitter<{ comId: string; mediaId: string }>();
 
-
   selectedMedia: any;
 
   goToMedia(mediaId: string) {
-    this.selectedMedia = this.media.find(item => item._id === mediaId);
+    this.selectedMedia = this.media.find((item) => item._id === mediaId);
   }
   goBack() {
     const currentIndex = this.media.indexOf(this.selectedMedia);
@@ -41,43 +49,52 @@ export class MediaGalleryComponent implements OnInit {
     this.close.emit();
   }
   onDelete() {
-    this.goForward();
-    this.goBack();
-    
-    this.media=this.media.filter((item)=> item._id !== this.selectedMedia._id)
-    this.delete.emit({ comId: this.selectedMedia.communicationId, mediaId: this.selectedMedia._id });
+    const currentIndex = this.media.indexOf(this.selectedMedia);
+    this.media = this.media.filter(
+      (item) => item._id !== this.selectedMedia._id
+    );
+    this.delete.emit({
+      comId: this.selectedMedia.communicationId,
+      mediaId: this.selectedMedia._id,
+    });
     if (this.media.length == 0) {
-      this.onClose()
+      this.onClose();
     }
-    
+    this.selectedMedia = this.media[Math.min(currentIndex, this.media.length-1)];
   }
 
   private swipeCoord?: [number, number];
   private swipeTime?: number;
   swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [
+      e.changedTouches[0].pageX,
+      e.changedTouches[0].pageY,
+    ];
+    const time = new Date().getTime();
 
-  const coord: [number, number] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
-  const time = new Date().getTime();
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [
+        coord[0] - this.swipeCoord![0],
+        coord[1] - this.swipeCoord![1],
+      ];
+      const duration = time - this.swipeTime!;
+      if (
+        duration < 1000 &&
+        Math.abs(direction[0]) > 30 &&
+        Math.abs(direction[0]) > Math.abs(direction[1] * 3)
+      ) {
+        if (direction[0] < 0) {
+          this.goForward();
+        } else {
+          this.goBack();
+        }
+      }
+    }
+  }
 
-  if (when === 'start') {
-    this.swipeCoord = coord;
-    this.swipeTime = time;
-  }
-  else if (when === 'end') {
-    const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
-    const duration = time - this.swipeTime!;
-  if (duration < 1000
-    && Math.abs(direction[0]) > 30 
-    && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { 
-    if (direction[0] < 0) {
-      this.goForward()
-    } else {
-      this.goBack()
-    }
-    }
-  }
-  }
-  
   ngOnInit() {
     if (this.media.length > 0) {
       this.selectedMedia = this.media[0];
