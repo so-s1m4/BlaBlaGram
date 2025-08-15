60e3e52fc596056303872ae3e0347e9048b1cf84
so-s1m4 <maks.rvachov.at@gmail.com>
2025-08-16 00:59:12 +0200

    fix of some design issues with create-space-pop-up

--- src/app/features/chats/ui/create-space-pop-up-modal/create-space-pop-up-modal.ts
+++ src/app/features/chats/ui/create-space-pop-up-modal/create-space-pop-up-modal.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChatsService } from '@features/chats/data/chats.service';
 import { FriendsService } from '@features/friends/data/friends.service';
+import { ProfileService } from '@features/profile/data/profile.service';
 import { ImgPipe } from '@shared/utils/img.pipe';
 import { SvgIconComponent } from '@shared/utils/svg.component';
 
@@ -11,6 +14,7 @@ import { SvgIconComponent } from '@shared/utils/svg.component';
   styleUrl: './create-space-pop-up-modal.css',
 })
 export class CreateModalPopUp {
+  AVAILABLE_TYPES = ['group', 'channel'];
   options = [
     {
       label: 'Create group',
@@ -26,8 +30,22 @@ export class CreateModalPopUp {
 
   window = window;
   private readonly friendsService = inject(FriendsService);
+  private readonly profileService = inject(ProfileService);
+  router = inject(Router);
+
   friends = this.friendsService.friends;
   type: string = '';
 
-  setType(type: string) {}
+  @Output() onClick = new EventEmitter();
+
+  select(option: string) {
+    if (this.AVAILABLE_TYPES.includes(option)) {
+      console.log(option)
+    } else {
+      this.profileService.openChat(option, (data: any) => {
+        this.onClick.emit()
+        this.router.navigate(['/chats'], { queryParams: { id: data.chat.id } });
+      });
+    }
+  }
 }
